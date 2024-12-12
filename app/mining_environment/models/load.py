3818b4e4f28181d854e3753e32f615c95c2890ac
John Doe <johndoe@example.com>
2024-12-12 07:45:50 +0000

    y

--- app/mining_environment/models/load.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-try:
-    model = tf.keras.models.load_model('cloaking_model.keras')
-    print("Mô hình đã được tải thành công.")
-    model.summary()
-except Exception as e:
-    print(f"Lỗi khi tải mô hình: {e}")
